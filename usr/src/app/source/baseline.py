4d081b6a03a860d0a157e3c8867ae6c33de9d5d7
Mitica Denis <miticadenis@Miticas-MacBook-Pro.local>
2024-11-09 23:05:11 +0200

    score 0.925

--- usr/src/app/source/baseline.py
+++ usr/src/app/source/baseline.py
@@ -93,8 +93,8 @@ def main():
     
     # Hyperparameter tuning using GridSearchCV
     param_grid = {
-        'n_estimators': [50, 100, 150],
-        'max_depth': [10, 20, 30],
+        'n_estimators': [50, 100, 150, 200, 250],
+        'max_depth': [50],
         'min_samples_split': [2, 5, 10],
         'min_samples_leaf': [1, 2, 4]
     }
